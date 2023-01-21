991af9c8b6a8e10f10bab3b77725f807b39f22fd
SmallPig1997 <664235822@qq.com>
2023-01-21 23:43:32 +0800

    设置项目名路径上下文

--- src/main/java/com/sims/springbootsims/ServletInitializer.java
+++ src/main/java/com/sims/springbootsims/ServletInitializer.java
@@ -2,7 +2,9 @@ package com.sims.springbootsims;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.sims.springbootsims")
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
